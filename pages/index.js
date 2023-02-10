cc77d7cdedee86cc8c3001b119a6658328755d89
Miguel Lorenzo <neutroflashp@hotmail.com>
2023-02-10 13:46:09 -0500

    Feature-Header

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
-
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import HomePage from "../content/HomePage";
+
+export default function Home(){
+    return(
+        <HomePage/>
+    )
+}
