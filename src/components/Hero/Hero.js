642dfa357d2ca563b13dfead12e2258cdddabde1
NeutroFlashp <neutroflashp@hotmail.com>
2023-02-24 09:02:39 -0500

    Avance-Hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,21 @@
+import { React } from "react";
+import { Filterbar } from '../FilterBar/Filterbar';
+import { Carousel } from "../Carousel/Carousel";
+import styles from './Hero.module.scss';
+
+export const Hero = () =>{
+    return(
+        <div className={styles.hero}>
+            <div className={styles.hero__title}>
+                <h1>Find an investment property</h1>
+            </div>
+            <div className={styles.hero__filterbar}>
+                <Filterbar/>
+            </div>
+            <div>
+                <Carousel/>
+            </div>
+        </div>
+    )
+}
+
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,25 @@
 import React from "react";
-/*import styles from './NavBar.module.scss';*/
+import styles from './NavBar.module.scss';
+import logo from '../../assets/logo-wealty.png';
+import { CgSearch } from 'react-icons/cg';
+import { FaUserAlt } from 'react-icons/fa';
 
 export const NavBar = () =>{
     return(
-        <div>
-           <ul>
-                <div>
-                    <img />
+        <div className={styles.navbar}>
+            <div className={styles.navbar__logo}>
+                <img  src={logo} alt="logo-wealty"/>
+            </div>
+            <div className={styles.navbar__items}>
+                <div className={styles.navbar__tools}>
+                    <div>
+                        <CgSearch className={styles.navbar__search} /><a href="./index.html">Search</a>
+                    </div>
+                    <div>
+                        <FaUserAlt className={styles.navbar__user} /><a href="./index.html">My Profile</a>
+                    </div>
                 </div>
-                <li><a href="./index.html">Search</a></li>
-                <li></li>
-           </ul>
+           </div>
         </div>
     )
 }
